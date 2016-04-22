9f6d046601059350864865e78478136e3854469c
Michael Wang <email": null>
2016-04-22 16:46:57 -0700

    geolocation to geokit object

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,5 @@ class Location < ActiveRecord::Base
                     :distance_field_name => :distance,
                     :lat_column_name => :lat,
                     :lng_column_name => :lng
+
 end
