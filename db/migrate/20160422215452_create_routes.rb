cb5c146fedddd617bd5ed1eab84185dea26eba0a
Ian Fricker <imfricker@gmail.com>
2016-04-25 12:25:17 -0700

    first update to users

--- db/migrate/20160422215452_create_routes.rb
+++ db/migrate/20160422215452_create_routes.rb
@@ -3,7 +3,6 @@ class CreateRoutes < ActiveRecord::Migration
     create_table :routes do |t|
       t.references :user, foreign_key: true
       t.decimal :distance
-      t.integer :start_id
       t.string :name
 
       t.timestamps null: false
