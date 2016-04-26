dda3fd416767b9699e374cd89c6115e3c72325c6
Alyssa Page & Michael Wang <pair+ajpage.michaelzwang@devbootcamp.com>
2016-04-25 19:50:44 -0700

    directions scroll fix

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 20160423014638) do
 
   create_table "routes", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "distance"
+    t.decimal  "distance"
+    t.integer  "start_id"
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
