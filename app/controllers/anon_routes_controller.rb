b542f11be183fd3d72bfd7a322081578812f729c
Alyssa Page <alyssa.j.page@gmail.com>
2016-05-09 10:55:24 -0700

    Change service class variable name

--- app/controllers/anon_routes_controller.rb
+++ app/controllers/anon_routes_controller.rb
@@ -7,21 +7,21 @@ class AnonRoutesController < ApplicationController
       params[:input_miles] = params[:input_miles] + "." + params[:input_miles_decimal]
     end
 
-    route_test = CreateRoute.new(params)
+    create_route = CreateRoute.new(params)
     new_route = Route.new()
 
-    start = route_test.get_start_location
+    start = create_route.get_start_location
     start.route = new_route
   
     new_route.start_id = start.id
     new_route.save
     start.save
 
-    route_test.set_route_direction
+    create_route.set_route_direction
 
-    route_test.create_route_waypoints(new_route)
+    create_route.create_route_waypoints(new_route)
 
-    new_route.map_url = route_test.make_map_url(new_route.waypoints)
+    new_route.map_url = create_route.make_map_url(new_route.waypoints)
     new_route.save
 
     respond_to do |format|
